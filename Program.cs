05171b844dad20fa0dcda66168423fcb885151c9
Undeadtaker <61665341+Undeadtaker@users.noreply.github.com>
2022-05-29 15:44:20 +0200

    Update #3
    + Now the MongoDB requires a username and password. Docker container now deployed with
    credentials.
    
    + Password is exposed through the UserSecretsId
    
    + Added new paths to check the health status of the website. There are two checks, the first one which checks the status
    of the API, and the second check which checks for the status of the MongoDB database service, which returns a JSON as a response.

--- Program.cs
+++ Program.cs
@@ -1,5 +1,8 @@
+using System.Net.Mime;
+using System.Text.Json;
 using Catalog.Collections;
 using Catalog.Config;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 using MongoDB.Bson;
 using MongoDB.Bson.Serialization;
 using MongoDB.Bson.Serialization.Serializers;
@@ -16,8 +19,20 @@ builder.Services.AddControllers(options =>
     options.SuppressAsyncSuffixInActionNames = false;
 });
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
+var mongoDbConfig = configuration.GetSection(nameof(MongoDBConfig)).Get<MongoDBConfig>(); 
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Check fails if not connected to DB for more than 5 seconds
+builder.Services.AddHealthChecks()
+        .AddMongoDb(
+            mongoDbConfig.ConnectionString, 
+            name: "mongodb", 
+            timeout: TimeSpan.FromSeconds(5),
+            tags: new[] {"ready"}
+        ); 
 
 
 // Tell the MongoDB how to serialize the Date and Guid format because it might end up serializing as false vars
@@ -26,8 +41,7 @@ BsonSerializer.RegisterSerializer(new DateTimeOffsetSerializer(BsonType.String))
 
 builder.Services.AddSingleton<IMongoClient>(serviceProvider => 
                 {
-                    var config = configuration.GetSection(nameof(MongoDBConfig)).Get<MongoDBConfig>(); 
-                    return new MongoClient(config.ConnectionString);
+                    return new MongoClient(mongoDbConfig.ConnectionString);
                 });
 
  builder.Services.AddSingleton<IInMemoryCollections, MongoDBCollections>();                
@@ -46,5 +60,32 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+
+app.MapHealthChecks("/health/ready", new HealthCheckOptions
+            {
+                Predicate = (check) => check.Tags.Contains("ready"),
+                ResponseWriter = async(context, report) => 
+                {
+                    var result = JsonSerializer.Serialize(
+                        new{
+                            status = report.Status.ToString(),
+                            checks = report.Entries.Select(entry => new{
+                                name = entry.Key,
+                                status = entry.Value.Status.ToString(),
+                                exception = entry.Value.Exception != null ? entry.Value.Exception.Message : "none",
+                                duration = entry.Value.Duration.ToString()
+                            })
+                        }
+                    );
+
+                    context.Response.ContentType = MediaTypeNames.Application.Json;
+                    await context.Response.WriteAsync(result);
+                }
+            });
+
+app.MapHealthChecks("/health/live", new HealthCheckOptions
+            {
+                Predicate = (_) => false 
+            });
 
 app.Run();
